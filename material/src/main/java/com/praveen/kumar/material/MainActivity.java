f0d05dce9343bc17b17118e62d06c34f140199f3
praveen kumar <programmerpraveenkumar@gmail.com>
2016-04-22 23:32:56 +0530

    animation

--- material/src/main/java/com/praveen/kumar/material/MainActivity.java
+++ material/src/main/java/com/praveen/kumar/material/MainActivity.java
@@ -2,12 +2,17 @@ package com.praveen.kumar.material;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
     }
+
 }
