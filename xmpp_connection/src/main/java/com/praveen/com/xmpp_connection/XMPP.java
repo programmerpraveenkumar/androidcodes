aea4ac0160cefbda22f7ba35e5f27cdc03ccb8ea
programmerpraveenkumar <programmerpraveenkumar@gmail.com>
2016-06-11 18:48:09 +0530

    xmpp connection

--- xmpp_connection/src/main/java/com/praveen/com/xmpp_connection/XMPP.java
+++ xmpp_connection/src/main/java/com/praveen/com/xmpp_connection/XMPP.java
@@ -2,12 +2,15 @@ package com.praveen.com.xmpp_connection;
 
 import android.os.AsyncTask;
 
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+
 /**
  * Created by gitpl-des-078 on 10-06-16.
  */
 public class XMPP{
     private String serverAddress;
-    private XMPPTCPConnection connection;
+    private XMPPConnection connection;
     private String loginUser;
     private String passwordUser;
 
@@ -21,7 +24,22 @@ public class XMPP{
         AsyncTask<Void, Void, Boolean> connectionThread = new AsyncTask<Void, Void, Boolean>(){
             @Override
             protected Boolean doInBackground(Void... arg0){
-                return true;
+                boolean isConnected = false;
+
+                ConnectionConfiguration config = new ConnectionConfiguration(serverAddress);
+                config.setReconnectionAllowed(true);
+
+                connection = new XMPPTCPConnection(config);
+
+                try{
+                    connection.connect();
+                    isConnected = true;
+                } catch (IOException e){
+                } catch (SmackException e){
+                } catch (XMPPException e){
+                }
+
+                return isConnected;
             }
         };
         connectionThread.execute();
