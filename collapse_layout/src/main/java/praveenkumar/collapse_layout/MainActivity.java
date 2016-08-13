7527b9219c040a583d2f5c964393687228568c05
praveen kumar <programmerpraveenkumar@gmail.com>
2016-08-13 20:21:38 +0530

    xmpp connection and collpase layout

---
+++ collapse_layout/src/main/java/praveenkumar/collapse_layout/MainActivity.java
@@ -0,0 +1,17 @@
+package praveenkumar.collapse_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //ListView list = (ListView) findViewById(R.id.list);
+
+        //list.setAdapter();
+    }
+}
